5b03002ae378a23008faeab8c06d939db9e3c49b
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-11-14 15:13:36 +0530

    change text

--- App.tsx
+++ App.tsx
@@ -1,14 +1,5 @@
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Text, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
