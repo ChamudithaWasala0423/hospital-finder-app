47379543fac58c2453d5e1e288861574816d0535
Kisara2001 <kisarar@gmail.com>
2023-11-21 22:19:59 +0530

    settings UI

--- App.tsx
+++ App.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import HomeScreen from './Screens/HomeScreen';
+import SettingScreen from './Screens/SettingScreen';
 
 function App(): JSX.Element {
-  return <HomeScreen />;
+  return <SettingScreen />;
 }
 
 export default App;
---
+++ Components/ButtonComponent.tsx
@@ -0,0 +1,48 @@
+// ButtonComponent.tsx
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+interface ButtonComponentProps {
+  backgroundColor: string;
+  borderRadius: number;
+  marginHorizontal: number;
+  fontColor: string;
+  borderColor: string;
+  onPress: () => void;
+  buttonText: string;
+}
+
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  backgroundColor,
+  borderRadius,
+  marginHorizontal,
+  fontColor,
+  borderColor,
+  onPress,
+  buttonText,
+}) => {
+  return (
+    <TouchableOpacity
+      style={{
+        ...styles.button,
+        backgroundColor,
+        borderRadius,
+        marginHorizontal,
+        borderColor,
+      }}
+      onPress={onPress}
+    >
+      <Text style={{ color: fontColor }}>{buttonText}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    borderWidth: 1,
+    alignItems: 'center',
+  },
+});
+
+export default ButtonComponent;
