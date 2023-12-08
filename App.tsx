cef4c46d1ca510089fe3d7b0dd8ddda7879cff6a
Tharuka Nanayakkara <tharuka.nmas@gmail.com>
2023-12-08 21:36:20 +0530

    set notification pop up screen

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import Country from './pop_upScreen/country';
 import Language from './pop_upScreen/language';
+import Notification from './pop_upScreen/notification';
 
 const App: React.FC = () => {
   return(
     <View>
-      <Language></Language>
+      <Notification></Notification>
     </View>
   );
 };
