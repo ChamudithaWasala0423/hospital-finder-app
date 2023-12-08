6bbc4297658b3a9f4be25507acd51398057130f5
Tharuka Nanayakkara <tharuka.nmas@gmail.com>
2023-12-08 22:01:07 +0530

    create Terms of use pop up screen

--- App.tsx
+++ App.tsx
@@ -4,11 +4,12 @@ import Country from './pop_upScreen/country';
 import Language from './pop_upScreen/language';
 import Notification from './pop_upScreen/notification';
 import Privacy from './pop_upScreen/privacy';
+import Terms from './pop_upScreen/terms';
 
 const App: React.FC = () => {
   return(
     <View>
-      <Privacy></Privacy>
+      <Terms></Terms>
     </View>
   );
 };
