2a06775afeb0af812f8349362d138531e3f6ee77
Chamuditha Wasala <chamudithaprabathwasala@gmail.com>
2023-12-09 14:26:25 +0530

    update: change login screen ui

--- Screens/Login.tsx
+++ Screens/Login.tsx
@@ -8,7 +8,7 @@ import {
   Image,
   TouchableOpacity,
   TextInput,
-  ImageBackground,
+  SafeAreaView,
 } from 'react-native';
 
 import ButtonComponent from '../Components/ButtonComponent';
@@ -55,196 +55,87 @@ const Login: React.FC = () => {
   };
 
   return (
-    <ImageBackground source={backgroundImage} style={styles.backgroundImage}>
-      <View style={styles.container}>
-        <Image
-          source={require('../assets/icon.png')}
-          style={styles.imageMain}
-        />
-        <View style={styles.textContainer}>
-          <Text style={styles.text}> Login to Your Account</Text>
-        </View>
-
-        <CardComponent
-          color="white"
-          width="80%"
-          height="45%"
-          marginTop={40}
-          marginBottom={20}
-          alignItems="center"
-          justifyContent="center">
-          <View style={{...styles.inputContainer}}>
-            <Text style={styles.textNormal}>Username</Text>
-            <TextInput
-              style={styles.textInput}
-              placeholderTextColor="#808080"
-              placeholder="Username"
-              value={email}
-              onChangeText={text => setEmail(text)}
-            />
-          </View>
-          <View style={{...styles.inputContainer}}>
-            <Text style={styles.textNormal}>Password</Text>
-            <View style={styles.passwordInputContainer}>
-              <TextInput
-                style={styles.textInput}
-                placeholderTextColor="#808080"
-                placeholder="Password"
-                secureTextEntry={!showPassword} // Use secureTextEntry based on the state
-                value={pass}
-                onChangeText={text => setPassword(text)}
-              />
-              <TouchableOpacity
-                onPress={handleTogglePassword}
-                style={styles.showPasswordButton}>
-                <EyeIcon size={24} color="black" />
-              </TouchableOpacity>
-            </View>
-          </View>
-        </CardComponent>
-
-        <ButtonComponent
-          backgroundColor="#0057E7"
-          borderRadius={30}
-          marginHorizontal={5}
-          fontColor="white"
-          borderColor="white"
-          onPress={handleLoginPress}
-          buttonText="Countinue"
-        />
-
-        {/* <TouchableOpacity style={styles.touchableContainer}>
-  <View style={styles.imagecontainer}>
-    <Image source={require('../assets/google.jpg')} style={styles.image} />
-    <Text style={styles.signtext}>Continue with Google</Text>
-  </View>
-</TouchableOpacity> */}
-
-        <TouchableOpacity
-          style={styles.touchableContainer}
-          onPress={() => navigation.navigate('PhoneVerification')}>
-          <View style={styles.imagecontainer}>
-            <Image
-              source={require('../assets/phone_icon.png')}
-              style={styles.image}
-            />
-            <Text style={styles.signtext}>Continue with Phone</Text>
-          </View>
-        </TouchableOpacity>
-
-        <ButtonComponent
-          backgroundColor="#0057E7"
-          borderRadius={30}
-          marginHorizontal={5}
-          fontColor="white"
-          borderColor="white"
-          onPress={() => navigation.navigate('SignUp')}
-          buttonText="Create Account"
-        />
+    <SafeAreaView style={styles.container}>
+      <View style={styles.MainBox}>
+        <Image source={require('../assets/send.png')} style={styles.image} />
+        <Text style={styles.titleText}>Welcome Back</Text>
       </View>
-    </ImageBackground>
+      <View style={styles.subContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="gray"
+          value={email}
+          onChangeText={text => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          placeholderTextColor="gray"
+          secureTextEntry={!showPassword}
+          value={pass}
+          onChangeText={text => setPassword(text)}
+        />
+
+        <TouchableOpacity style={styles.mainButton} onPress={handleLoginPress}>
+          <Text style={styles.signinButton}>Sign in</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
-    //flexDirection:'row'
+    backgroundColor: '#f2f4f5',
   },
-
-  textContainer: {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-  },
-
-  text: {
-    fontSize: 25,
-    color: '#000000',
-    fontWeight: 'bold',
-    marginTop: 25,
-  },
-  createText: {
-    fontSize: 16,
-    color: '#707370',
-    marginTop: 50,
-  },
-  inputContainer: {
-    padding: 10,
-
-    width: '80%',
-    backgroundColor: 'rgba(255, 255, 255, 0.5)',
-
-    alignItems: 'center', // Align items in the center vertically// Adjust the alpha (fourth value) as needed
-  },
-  textNormal: {
-    fontSize: 20,
-    color: '#000000',
-    fontWeight: 'bold',
-    marginVertical: 5,
-  },
-  textInput: {
-    padding: 10,
-    fontSize: 15,
-    borderWidth: 1,
-    color: '#000000',
-    borderColor: '#a7a7a7',
-    borderRadius: 10,
+  MainBox: {
     width: '100%',
-  },
-  touchableContainer: {
-    justifyContent: 'center', // Center vertically
-    alignItems: 'center', // Center horizontally
-  },
-  imagecontainer: {
-    flexDirection: 'row', // Arrange children horizontally
-    justifyContent: 'center', // Center children horizontally
+    height: 250,
+    // backgroundColor: 'red',
     alignItems: 'center',
-    backgroundColor: 'white',
-    padding: 5,
-    borderRadius: 50,
-    marginTop: 10,
-    marginBottom: 10, // Center children vertically
-  },
-  imageMain: {
-    width: 70, // Set the desired width
-    height: 70,
-    borderRadius: 50,
-    padding: 20,
-
-    // Set the desired height
+    justifyContent: 'center',
   },
   image: {
-    width: 30, // Set the desired width
-    height: 30,
-    borderRadius: 50, // Set the desired height
+    width: 50,
+    height: 50,
   },
-  signtext: {
-    fontSize: 15,
-    color: '#000000',
-    justifyContent: 'center', // Center children horizontally
+  titleText: {
+    fontSize: 25,
+    color: 'black',
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  subContainer: {
+    width: '100%',
+    height: 350,
+    // backgroundColor: 'blue',
     alignItems: 'center',
-    marginTop: 0,
-    marginLeft: 15,
-    marginRight: 5,
   },
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
+  input: {
+    height: '15%',
+    width: '85%',
+    borderColor: 'gray',
+    borderWidth: 2,
+    marginBottom: 10,
+    paddingLeft: 10,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  mainButton: {
+    width: '85%',
+    height: '15%',
+    backgroundColor: '#0057e7',
+    borderRadius: 10,
+    alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 10,
   },
-  passwordInputContainer: {
-    flexDirection: 'row',
-    position: 'relative',
-  },
-  showPasswordButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
+  signinButton: {
+    fontSize: 15,
+    color: 'white',
+    fontWeight: 'bold',
   },
 });
 export default Login;
